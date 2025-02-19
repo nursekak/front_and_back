49f14df1046843145e4a5a7f4bf04ca9844f759f
nursekak <146924409+nursekak@users.noreply.github.com>
2025-02-19 17:51:42 +0300

    react moy

---
+++ react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Привет, мир!</h1>
+        <p>Это мой первый React проект.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
